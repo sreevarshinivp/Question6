6de4b7e534160d18b78721163cb22147fb77052d
sreevarshinivp <148671907+sreevarshinivp@users.noreply.github.com>
2023-10-22 12:28:37 +0530

    Update quote.js

--- quote.js
+++ quote.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002 ;
 
 // Define a list of inspirational quotes (you can replace this with your own list).
 const inspirationalQuotes = [
   'You did not come this far to come ONLY this far!-Move forward!',
-  'The mind alone is one's friend and enemy at the same time.',
-  'Believe you can and you’re halfway there.',
+  'You were given this life because you are strong enough to live it!.',
+  'Believe you can and you are halfway there.',
   'You are never too old to set another goal or dream a new dream',
   'Have patience- all things are difficult before they become easy.',
-  'As you believe, so you become.',
+  'Do not limit your challenges. Rather, challenge your limits!',
 ];
 
 // Endpoint to get a random inspirational quote.
